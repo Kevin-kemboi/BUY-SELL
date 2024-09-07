c0525f9d578e6b89fcd6e3cefc0c6331df45a23b
zzzzshawn <zzzzshawn@gmail.com>
2024-09-07 23:22:47 +0530

    aight

--- zin/src/zin-frontend/_root/pages/AllProducts.jsx
+++ zin/src/zin-frontend/_root/pages/AllProducts.jsx
@@ -2,6 +2,7 @@ import { getProductsFrontend } from "@/lib/api/api";
 import LeftSidebar from "@/zin-frontend/components/LeftSidebar";
 import RightSidebar from "@/zin-frontend/components/RightSidebar";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const AllProducts = () => {
   const [products, setProducts] = useState([]);
@@ -21,20 +22,20 @@ const AllProducts = () => {
   return (
     <div className=" max-w-6xl flex mx-auto">
       <LeftSidebar />
-      <div className="w-full mx-auto px-6 grid grid-cols-3   ">
+      <div className="w-full mx-auto h-max px-6 grid grid-cols-3 gap-2">
         {products.map((product) => (
-          <figure key={product._id}
-            className="relative   cursor-pointer overflow-hidden rounded-md border  border-dark-4 bg-dark-1 flex justify-center items-center hover:border-blue-500"
+          <Link to={`/product/${product._id}`} key={product._id}
+            className="relative p-7  cursor-pointer overflow-hidden rounded-md border  border-dark-4 bg-dark-1 flex justify-center items-center hover:border-blue-500 "
           >
             <img
               src={product.imageUrl}
               className=" size-[95%] object-contain hover:scale-110 transition-all duration-300"
             />
             <div className="z-10 absolute bottom-5 left-3 mx-2 flex gap-2 border border-light-2/30 pl-3 pr-1 py-1 rounded-full bg-dark-1">
-              <p className="px-2">{name}</p>
-              <p className="bg-blue-500 rounded-full px-2">₹{product.price}</p>
+              <p className="px-2 text-xs line-clamp-1">{product.name}</p>
+              <p className="bg-blue-500 rounded-full text-xs px-2">₹{product.price}</p>
             </div>
-          </figure>
+          </Link>
         ))}
       </div>
       <RightSidebar />
--- zin/src/zin-frontend/components/RightSidebar.jsx
+++ zin/src/zin-frontend/components/RightSidebar.jsx
@@ -2,7 +2,7 @@ import {  sortOptions } from '@/lib/constants'
 
 const RightSidebar = () => {
   return (
-    <div className='border-r border-dark-4 min-h-screen w-[200px] px-1 flex flex-col gap-2'>
+    <div className='border-l border-dark-4 min-h-screen w-[200px] px-4 flex flex-col gap-2'>
         <h2 className='text-[10px] text-dark-5/70'>SORT BY</h2>
         <div className=" w-full flex flex-col gap-0.5">
             {
