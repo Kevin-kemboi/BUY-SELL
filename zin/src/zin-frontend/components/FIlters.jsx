b7a2cf59faa0d4eef074b26447eb55e2a7bba044
zzzzshawn <zzzzshawn@gmail.com>
2024-09-08 12:29:24 +0530

    aight

--- zin/src/zin-frontend/components/FIlters.jsx
+++ zin/src/zin-frontend/components/FIlters.jsx
@@ -1,9 +1,27 @@
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { formUrlQuery } from "@/lib/utils";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Filters = ({ filters = [] }) => {
+    const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    const paramsFilter = searchParams.get("filter");
+
+    const handleUpdateParams = (value) => {
+        const newUrl = formUrlQuery({
+            params: searchParams.toString(),
+            key: "filter",
+            value,
+        });
+        navigate(newUrl)
+
+    }
+
+
   return (
-    <RadioGroup defaultValue={``} className="gap-1">
+    <RadioGroup defaultValue={paramsFilter} onValueChange={handleUpdateParams}  className="gap-1">
       {filters.map((filter) => (
         <div key={filter.value} className="flex items-center space-x-1">
           <RadioGroupItem value={filter.value} id={filter.name} />
