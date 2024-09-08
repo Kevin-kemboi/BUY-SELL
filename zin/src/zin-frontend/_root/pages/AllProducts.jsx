6c963f9fe2db4e16c647eab8cd196a472b1c55f2
zzzzshawn <zzzzshawn@gmail.com>
2024-09-08 10:12:09 +0530

    aight

--- zin/src/zin-frontend/_root/pages/AllProducts.jsx
+++ zin/src/zin-frontend/_root/pages/AllProducts.jsx
@@ -22,8 +22,8 @@ const AllProducts = () => {
   return (
     <div className=" max-w-6xl flex mx-auto">
       <LeftSidebar />
-      <div className="w-full mx-auto h-max px-6 grid xl:grid-cols-3 md:grid-cols-2  gap-2">
-        {products.map((product) => (
+      <div className="w-full mx-auto h-max px-6 grid md:grid-cols-3 grid-cols-2  gap-2">
+        {products.map((product) => ( 
           <Link to={`/product/${product._id}`} key={product._id}
             className="relative p-7 h-[250px] xl:h-[300px] cursor-pointer overflow-hidden rounded-md border  border-dark-4 bg-dark-1 flex justify-center items-center hover:border-blue-500 "
           >
@@ -31,15 +31,15 @@ const AllProducts = () => {
               src={product.imageUrl}
               className=" size-[95%] object-contain hover:scale-110 transition-all duration-300"
             />
-            <div className="z-10 absolute bottom-5 left-3 mx-2 flex gap-2 border border-light-2/30 pl-3 pr-1 py-1 rounded-full bg-dark-1">
-              <p className="px-2 text-xs line-clamp-1">{product.name}</p>
+            <div className="z-10 absolute bottom-5 left-3 mx-2 flex gap-1 border border-light-2/20 pl-3 pr-1 py-1 rounded-full bg-dark-1">
+              <p className="pr-1 text-xs line-clamp-1">{product.name}</p>
               <p className="bg-blue-500 rounded-full text-xs px-2">₹{product.price}</p>
             </div>
           </Link>
         ))}
       </div>
       <RightSidebar />
-    </div>
+    </div> 
   );
 };
 
--- zin/src/zin-frontend/components/LeftSidebar.jsx
+++ zin/src/zin-frontend/components/LeftSidebar.jsx
@@ -2,7 +2,7 @@ import { categories } from '@/lib/constants'
 
 const LeftSidebar = () => {
   return (
-    <div className='border-r border-dark-4 min-h-screen w-[200px] px-1 flex flex-col gap-2'>
+    <div className='border-r border-dark-4 h-[95vh] w-[200px] px-1 flex flex-col gap-2'>
         <h2 className='text-[10px] text-dark-5/70'>COLLECTIONS</h2>
         <div className=" w-full flex flex-col gap-0.5">
             {
